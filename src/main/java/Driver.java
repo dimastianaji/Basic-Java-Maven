5def25b1e2547ae96a38e227baafc5953b33b628
Dimastian Aji Wibowo <147970502+dimastianaji@users.noreply.github.com>
2026-06-14 15:20:25 +0700

    Refactor main method in Driver class
    Updated main method to use increaseBy and decreaseBy methods, and added multy method call.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,20 +2,24 @@ import src.main.java.Counter;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Current Count:" + counter.getCount());
-		
-		counter.increment();
-		
-		System.out.println("Current Count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Current Count:" + counter.getCount());
-		
-	}
+    public static void main(String[] args) {
+        
+        Counter counter = new Counter();
+        
+        System.out.println("Current Count: " + counter.getCount());
+        
+        counter.increaseBy(5); 
+        
+        System.out.println("Current Count: " + counter.getCount()); 
+        
+        counter.decreaseBy(2);
+        
+        System.out.println("Current Count: " + counter.getCount());
+
+        counter.multy(2);
+        
+        System.out.println("Current Count: " + counter.getCount()); 
+        
+    }
 
 }
