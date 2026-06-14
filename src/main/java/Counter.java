55544693b236e972562cf880a4f9b4e4fdc48717
Dimastian Aji Wibowo <147970502+dimastianaji@users.noreply.github.com>
2026-06-14 15:19:14 +0700

    Update Counter.java

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -2,26 +2,38 @@ package src.main.java;
 
 public class Counter {
 
-	private int count;
-	
-	public Counter() {
-		this.reset();
-	}
-		
-	public void reset() {
-		count = 0;
-	}
-	
-	public void increment() {
-		count++;
-	}
-	
-	public void decrement() {
-		count--;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
+    private int count;
+    
+    public Counter() {
+        this.reset();
+    }
+        
+    public void reset() {
+        count = 0;
+    }
+    
+    public void increment() {
+        count++;
+    }
+    
+    public void decrement() {
+        count--;
+    }
+
+    public void increaseBy(int amount) {
+        count += amount;
+    }
+
+    public void decreaseBy(int amount) {
+        count -= amount;
+    }
+
+    public void multy(int factor) {
+        count *= factor;
+    }
+    
+    public int getCount() {
+        return count;
+    }
+    
 }
